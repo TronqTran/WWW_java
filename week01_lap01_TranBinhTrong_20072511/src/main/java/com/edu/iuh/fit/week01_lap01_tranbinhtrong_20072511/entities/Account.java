9e41576c1ccdde62b430e334680377f789feb18a
Your Name <you@example.com>
2023-10-12 14:51:21 +0700

    week1

---
+++ week01_lap01_TranBinhTrong_20072511/src/main/java/com/edu/iuh/fit/week01_lap01_tranbinhtrong_20072511/entities/Account.java
@@ -0,0 +1,79 @@
+package com.edu.iuh.fit.week01_lap01_tranbinhtrong_20072511.entities;
+
+public class Account {
+    private String account_id;
+    private String fullName;
+    private String password;
+    private String email;
+    private String phone;
+    private int status;
+
+    public Account(String account_id, String fullName, String password, String email, String phone, int status) {
+        this.account_id = account_id;
+        this.fullName = fullName;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.status = status;
+    }
+
+    public String getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(String account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "account_id='" + account_id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
---
+++ week1/week01_lap01_TranBinhTrong_20072511/src/main/java/com/edu/iuh/fit/week01_lap01_tranbinhtrong_20072511/entities/Logs.java
@@ -0,0 +1,2 @@
+package com.edu.iuh.fit.week01_lap01_tranbinhtrong_20072511.entities;public class Logs {
+}
